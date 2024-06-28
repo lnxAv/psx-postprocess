309f929b278b52cef7621f4754f88cf22a81864d
Lnx <46533182+lnxAv@users.noreply.github.com>
2024-06-27 23:57:43 -0400

    Feature: Rename+Add CRTEffect+Add Vertex snapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import MainCanvas from '../components/mainCanvas.jsx'
+import MainCanvas from '../components/mainCanvas'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen h-screen w-screen">
-      <MainCanvas />
+      <MainCanvas resolution={[320, 240]} />
     </main>
   );
 }
--- src/components/mainCanvas.tsx
+++ src/components/mainCanvas.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense, useEffect, useLayoutEffect } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { Rebeca } from "./Rebeca-bones";
-import { MathUtils, SRGBColorSpace, Vector2 } from "three";
+import { MathUtils, SRGBColorSpace, Vector2, ShaderChunk, Texture } from "three";
 import { CameraShake, PerspectiveCamera, useTexture } from "@react-three/drei";
 import {  EffectComposer, ToneMapping, Vignette } from '@react-three/postprocessing'
 import { Dithering, DitheringPattern } from "./Effects/Dithering";
 import { BlendFunction, ToneMappingMode } from "postprocessing";
+import { CRTMonitor } from "./Effects/CRTMonitor";
 
-
-function cover( texture, aspect ) {
+function cover( texture: Texture, aspect: number ) {
     let imageAspect = texture.image.width / texture.image.height;
     texture.matrixAutoUpdate = false;
     if ( aspect < imageAspect ) {
@@ -33,16 +33,48 @@ const Background = ()=>{
     return null
 }
 
-function MainCanvas() {
+type PSXCanvasProps = {
+    resolution: [number, number]
+}
+
+function MainCanvas({resolution = [320, 240]}: PSXCanvasProps) {
+    //~ MODIFY ENGINE VERTEX SHADER + TEXTURE MAPPING (by romanliutikov)
+    useLayoutEffect(()=>{
+        ShaderChunk.project_vertex = /* glsl */`
+            vec4 mvPosition = vec4( transformed, 1.0 );
+
+            // KEEP BATCHING (from three.js)
+            #ifdef USE_BATCHING
+                mvPosition = batchingMatrix * mvPosition;
+            #endif
+            // KEEP INSTANCING (from three.js)
+            #ifdef USE_INSTANCING
+                mvPosition = instanceMatrix * mvPosition;
+            #endif
+
+            mvPosition = modelViewMatrix * mvPosition;
+
+            vec2 resolution = vec2(${resolution[0]}, ${resolution[1]});
+            vec4 pos = projectionMatrix * mvPosition;
+            float i = (1.0 - 0.5) * min(pos.x, pos.y);
+            pos.xyz /= pos.w;
+            pos.xy = (floor(resolution * pos.xy) ) / resolution ;
+            pos.xyz *= pos.w;
+
+            gl_Position = pos;
+        `;
+    }, [])
+
+
     return(
-    <Canvas className="w-screen min-h-screen" dpr={0.5} gl={{antialias: 'false'}}>
+    <Canvas className="w-screen min-h-screen" dpr={0.25} gl={{antialias: false}} style={{imageRendering: 'pixelated'}}>
         <Background/>
         <PerspectiveCamera makeDefault={true} position={[-0.05, 0, 1]} />
         <CameraShake intensity={1} maxPitch={0.1} maxYaw={0.1} maxRoll={0.1} />
         <EffectComposer>
             <ToneMapping mode={ToneMappingMode.NEUTRAL} />
             <Dithering pattern={DitheringPattern.BAYER_4} darkness={0.5} colorDepth={16} blendFunction={BlendFunction.SCREEN}/>
-            <Vignette eskil={false} offset={0.1} darkness={0.5} />
+            <CRTMonitor/>
         </EffectComposer>
         <spotLight intensity={Math.PI * 1.5} position={[-1, -1, 1.5]} rotation={[1,-2,-1]} castShadow />
         <directionalLight intensity={0.2} position={[8, 20, 8]} castShadow />
